e0c1057c8c8ae295ecdb86d064202727437f9c02
deadprogram <ron@hybridgroup.com>
2023-09-10 22:52:11 +0200

    musicjam/arduino: arpeggio controller with display
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ musicjam/arduino/arpeggio-display/display.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"machine"
+
+	"image/color"
+	"time"
+
+	"tinygo.org/x/drivers/ssd1306"
+	"tinygo.org/x/tinydraw"
+	"tinygo.org/x/tinyfont"
+	"tinygo.org/x/tinyfont/freemono"
+)
+
+const (
+	startx       = 10
+	starty       = 20
+	radius int16 = 6
+)
+
+func handleDisplay() {
+	machine.I2C0.Configure(machine.I2CConfig{
+		Frequency: machine.TWI_FREQ_400KHZ,
+	})
+
+	display := ssd1306.NewI2C(machine.I2C0)
+	display.Configure(ssd1306.Config{
+		Address: ssd1306.Address_128_32,
+		Width:   128,
+		Height:  64,
+	})
+
+	display.ClearDisplay()
+
+	black := color.RGBA{1, 1, 1, 255}
+
+	for {
+		display.ClearBuffer()
+		msg := ""
+
+		for i, key := range keys {
+			if key.pressed {
+				tinydraw.FilledCircle(&display, startx+radius+32*int16(i), 48-radius-1, radius, black)
+				msg = key.chord.name
+			} else {
+				tinydraw.Circle(&display, startx+radius+32*int16(i), 48-radius-1, radius, black)
+			}
+		}
+
+		tinyfont.WriteLine(&display, &freemono.Bold9pt7b, startx, starty, msg, black)
+
+		display.Display()
+
+		time.Sleep(200 * time.Millisecond)
+	}
+}
---
+++ musicjam/arduino/arpeggio-display/main.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"machine"
+	"machine/usb/adc/midi"
+
+	"time"
+)
+
+type chord struct {
+	name  string
+	notes []midi.Note
+}
+
+var (
+	led                 = machine.LED
+	buttonC machine.Pin = machine.D12
+	buttonE machine.Pin = machine.D11
+	buttonG machine.Pin = machine.D10
+	buttonB machine.Pin = machine.D9
+
+	chords = []chord{
+		{name: "C ", notes: []midi.Note{midi.C3, midi.E3, midi.G3}},
+		{name: "G ", notes: []midi.Note{midi.G3, midi.B3, midi.D4}},
+		{name: "Am", notes: []midi.Note{midi.A3, midi.C4, midi.E4}},
+		{name: "F ", notes: []midi.Note{midi.F3, midi.A3, midi.C4}},
+	}
+
+	keys = []key{
+		{name: "C", pin: buttonC, chord: chords[0]},
+		{name: "G", pin: buttonE, chord: chords[1]},
+		{name: "A", pin: buttonG, chord: chords[2]},
+		{name: "F", pin: buttonB, chord: chords[3]},
+	}
+
+	midicable   uint8 = 0
+	midichannel uint8 = 1
+	velocity    uint8 = 0x40
+
+	// change this to speed up or slow down the music
+	bpm int = 80
+
+	currentNote  int = 0
+	currentNotes []midi.Note
+)
+
+func main() {
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	buttonC.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	buttonE.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	buttonG.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+	buttonB.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
+
+	go handleKeys()
+	go handleDisplay()
+
+	for {
+		playArpeggio()
+		time.Sleep(time.Duration(60000/bpm/4) * time.Millisecond)
+	}
+}
+
+func handleKeys() {
+	for {
+		for i := range keys {
+			switch {
+			case keys[i].press():
+				led.High()
+
+				startArpeggio(keys[i].chord.notes)
+
+				keys[i].pressed = true
+
+			case keys[i].release():
+				led.Low()
+
+				stopArpeggio()
+
+				keys[i].pressed = false
+			}
+		}
+
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+type key struct {
+	name    string
+	pin     machine.Pin
+	chord   chord
+	pressed bool
+}
+
+func (k key) press() bool {
+	if !k.pin.Get() && !k.pressed {
+		return true
+	}
+	return false
+}
+
+func (k key) release() bool {
+	if k.pin.Get() && k.pressed {
+		return true
+	}
+	return false
+}
+
+func startArpeggio(notes []midi.Note) {
+	if currentNotes != nil {
+		stopArpeggio()
+	}
+
+	currentNotes = notes
+
+	midi.Port().NoteOn(midicable, midichannel, currentNotes[currentNote], velocity)
+}
+
+func stopArpeggio() {
+	if currentNotes == nil {
+		return
+	}
+
+	midi.Port().NoteOff(midicable, midichannel, currentNotes[currentNote], velocity)
+
+	currentNotes = nil
+}
+
+func playArpeggio() {
+	if currentNotes == nil {
+		return
+	}
+
+	midi.Port().NoteOff(midicable, midichannel, currentNotes[currentNote], velocity)
+
+	currentNote++
+	if currentNote >= len(currentNotes) {
+		currentNote = 0
+	}
+
+	midi.Port().NoteOn(midicable, midichannel, currentNotes[currentNote], velocity)
+}
